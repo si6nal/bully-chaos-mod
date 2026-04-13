d672eeb63033d2e013ca1a0b3ffa39f1edc113f7
signal <83561323+si6nal@users.noreply.github.com>
2026-04-13 12:45:09 -0600

    invincibility event reverts to original health

--- src/game/mods/health.rs
+++ src/game/mods/health.rs
@@ -6,10 +6,19 @@ use crate::memory::{game_offsets, memory};
 const DEFAULT_MAX_HEALTH: f32 = 200f32;
 
 pub async fn give_invincibility(data: &GameData) {
+    // get current health (we will revert to it)
+    let original_health = get_health(&data);
+
+    // get current time for duration check
     let start_time = Instant::now();
     loop {
         // check if 30 seconds has passed
         if start_time.elapsed().as_secs() >= 30 {
+            // revert to original health
+            if let Some(original_health) = original_health {
+                update_health(&data, original_health);
+            }
+
             break;
         }
 
@@ -51,6 +60,16 @@ pub fn no_health(data: &GameData) {
 
 pub fn suicide(data: &GameData) {
     update_health(&data, 0f32);
+}
+
+fn get_health(data: &GameData) -> Option<f32> {
+    match game_offsets::get_offset(data.handle, data.player_offset, game_offsets::PLAYER_HEALTH_OFFSET) {
+        Some(health_offset) => memory::read_float(data.handle, health_offset),
+        None => {
+            warn!("failed to get health offset.");
+            None
+        }
+    }
 }
 
 fn update_health(data: &GameData, health: f32) {
