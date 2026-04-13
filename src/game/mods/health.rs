c95ebf6cf5a6fe946f805becc770a98f41af3899
signal <83561323+si6nal@users.noreply.github.com>
2026-04-13 12:46:58 -0600

    optimized invincibility event

--- src/game/mods/health.rs
+++ src/game/mods/health.rs
@@ -9,6 +9,9 @@ pub async fn give_invincibility(data: &GameData) {
     // get current health (we will revert to it)
     let original_health = get_health(&data);
 
+    // get max health
+    let max_health = get_max_health(&data);
+
     // get current time for duration check
     let start_time = Instant::now();
     loop {
@@ -22,8 +25,8 @@ pub async fn give_invincibility(data: &GameData) {
             break;
         }
 
-        // heal player
-        heal(&data);
+        // set health to max
+        update_health(&data, max_health);
 
         // sleep for cpu usage
         tokio::time::sleep(Duration::from_millis(100)).await;
@@ -32,23 +35,7 @@ pub async fn give_invincibility(data: &GameData) {
 
 pub fn heal(data: &GameData) {
     // get max health
-    let max_health: f32 = match game_offsets::get_offset(data.handle, data.player_offset, game_offsets::PLAYER_MAX_HEALTH_OFFSET) {
-        Some(max_health_offset) => {
-            // read max health value
-            match memory::read_float(data.handle, max_health_offset) {
-                Some(max_health) => {
-                    // check if the read failed
-                    if max_health <= 0f32 {
-                        DEFAULT_MAX_HEALTH
-                    } else {
-                        max_health
-                    }
-                },
-                None => DEFAULT_MAX_HEALTH
-            }
-        },
-        None => DEFAULT_MAX_HEALTH
-    };
+    let max_health = get_max_health(&data);
 
     // set health to max health
     update_health(&data, max_health);
@@ -69,6 +56,26 @@ fn get_health(data: &GameData) -> Option<f32> {
             warn!("failed to get health offset.");
             None
         }
+    }
+}
+
+fn get_max_health(data: &GameData) -> f32 {
+    match game_offsets::get_offset(data.handle, data.player_offset, game_offsets::PLAYER_MAX_HEALTH_OFFSET) {
+        Some(max_health_offset) => {
+            // read max health value
+            match memory::read_float(data.handle, max_health_offset) {
+                Some(max_health) => {
+                    // check if the read failed
+                    if max_health <= 0f32 {
+                        DEFAULT_MAX_HEALTH
+                    } else {
+                        max_health
+                    }
+                },
+                None => DEFAULT_MAX_HEALTH
+            }
+        },
+        None => DEFAULT_MAX_HEALTH
     }
 }
 
