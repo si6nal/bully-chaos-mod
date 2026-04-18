c20762478daecd8aad3f9b2f10387dc80a77681b
signal <83561323+si6nal@users.noreply.github.com>
2026-04-18 11:18:01 -0600

    added sanity loops to collecting game data
    also changed/added logging for required variables

--- src/game/bully.rs
+++ src/game/bully.rs
@@ -1,4 +1,4 @@
-use log::info;
+use log::{info, warn};
 use windows::Win32::Foundation::{HANDLE, HWND};
 use crate::memory::game_offsets;
 use crate::windows::{processes, window};
@@ -19,6 +19,7 @@ impl GameData {
             match processes::get_process_id("Bully.exe") {
                 Some(pid) => {
                     bully_pid = pid;
+                    info!("bully pid: {}", bully_pid);
                     break;
                 },
                 None => {
@@ -28,33 +29,58 @@ impl GameData {
             }
         }
 
-        info!("bully pid: {}", bully_pid);
-
         // get window handle for bully
         let window_handle: HWND;
         loop {
             match window::get_window_handle("Bully") {
                 Some(bully_hwnd) => {
                     window_handle = bully_hwnd;
+                    info!("successfully opened handle to bully window.");
                     break;
                 },
                 None => {
-                    tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
-                    info!("failed to get window handle, sleeping...");
+                    warn!("failed to get window handle, sleeping...");
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
                 },
             }
         }
 
         // todo: wait for game to initialize (fix crash)
 
-        info!("window handle: {:?}", window_handle);
-
         // get module address of the executable
-        let base_addr = processes::get_module_address(bully_pid, "Bully.exe").expect("failed to get base address of game.");
-        info!("base addr: 0x{:08x}", base_addr);
+        let base_addr: usize;
+        loop {
+            match processes::get_module_address(bully_pid, "Bully.exe") {
+                Some(bully_base_addr) => {
+                    base_addr = bully_base_addr;
+                    info!("base address: 0x{:08x}", base_addr);
+                    break;
+                },
+                None => {
+                    warn!("failed to get base address of bully, sleeping...");
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                }
+            }
+        }
 
         // open handle to game
-        let handle = processes::open_process(bully_pid).expect("failed to get handle to game.");
+        let handle: HANDLE;
+        loop {
+            match processes::open_process(bully_pid) {
+                Some(bully_handle) => {
+                    handle = bully_handle;
+                    info!("successfully opened handle to bully.");
+                    break;
+                },
+                None => {
+                    warn!("failed to open handle to bully process, sleeping...");
+                    tokio::time::sleep(std::time::Duration::from_secs(1)).await;
+                }
+            }
+        }
+
+        // sleep for 1 second to allow the game to complete initializing
+        //tokio::time::sleep(std::time::Duration::from_secs(1)).await;
 
         // get offset of player
         let player_offset = game_offsets::get_player_offset(base_addr);
